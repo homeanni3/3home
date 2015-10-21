affd1f167c8caa8728398bfe41567837c13295fc
homeanni3 <ynccjxkl@vomoto.com>
2015-10-21 16:07:51 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# For OS X-based systems.
+
+clear
+echo "Installing all Google Web Fonts onto your Mac"
+echo "Downloading the fonts..."
+cd ~/Desktop/
+echo "hey Marco come stai?"
+say -o hey marco come stai?
